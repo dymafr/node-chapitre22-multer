772f46c929f1c2f0f60071771b923dec80d75c05
dymafr <contact@dyma.fr>
2021-03-27 14:22:24 +0100

    feat: maj

--- app.js
+++ app.js
@@ -4,24 +4,23 @@ const app = express();
 const morgan = require('morgan');
 const multer = require('multer');
 const User = require('./database/models/user.model');
-const upload = multer(
-  { 
-    storage: multer.diskStorage({
-      destination: (req, file, cb) => {
-        cb(null, path.join( __dirname, '/upload'))
-      },
-      filename: (req, file, cb) => {
-        cb(null, Date.now() + '-' + file.originalname);
-      }
-    }), 
-    limits: {
-      fileSize: 10000
-    }, 
-    fileFilter: (req, file, cb) => {
-      console.log(file);
-      cb(null, true)
-    }
-  })
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, path.join(__dirname, '/upload'));
+    },
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + '-' + file.originalname);
+    },
+  }),
+  limits: {
+    fileSize: 10000,
+  },
+  fileFilter: (req, file, cb) => {
+    console.log(file);
+    cb(null, true);
+  },
+});
 
 const util = require('util');
 require('./database');
@@ -38,35 +37,52 @@ app.use(morgan('tiny'));
 app.get('/', async (req, res) => {
   try {
     const users = await User.find({}).exec();
-    console.log(users);
-    const user = users && users.length ? users[1] : null
+    const user = users && users.length ? users[0] : null;
     res.render('index', { user });
-
-  } catch(e) {
+  } catch (e) {
     next(e);
   }
-})
+});
 
-app.post('/file', upload.single('avatar'), async (req, res) => {
-  console.log(util.inspect(req.body, { compact: false, depth: 5, breakLength: 80, color: true }));
-  console.log(util.inspect(req.files, { compact: false, depth: 5, breakLength: 80, color: true }));
+app.post('/file', upload.single('avatar'), async (req, res, next) => {
+  console.log(
+    util.inspect(req.body, {
+      compact: false,
+      depth: 5,
+      breakLength: 80,
+      color: true,
+    })
+  );
+  console.log(
+    util.inspect(req.files, {
+      compact: false,
+      depth: 5,
+      breakLength: 80,
+      color: true,
+    })
+  );
 
   try {
     const newUser = new User({
-      avatar: req.file.filename
-    })
-    const savedUser = await newUser.save();
+      avatar: req.file.filename,
+    });
+    await newUser.save();
     res.redirect('/');
-  } catch(e) {
+  } catch (e) {
     next(e);
   }
-  
-})
+});
 
 app.use((err, req, res, next) => {
-  console.log(util.inspect(err, { compact: false, depth: 5, breakLength: 80, color:true }));
+  console.log(
+    util.inspect(err, {
+      compact: false,
+      depth: 5,
+      breakLength: 80,
+      color: true,
+    })
+  );
   res.status(500).redirect('/');
-})
-
+});
 
 app.listen(3000);
